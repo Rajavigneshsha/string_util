28fef3d007f946508a45578429e6a26af1b77684
Rajavignesh <rajavignesh.kandhaswami@treselle.com>
2017-06-30 15:27:35 +0530

    commit

--- src/com/discernnetwork/jenkins_stringutil/util/StringUtil.java
+++ src/com/discernnetwork/jenkins_stringutil/util/StringUtil.java
@@ -1,8 +1,8 @@
-package com.discernnetwork.jenkins.util;
+package com.discernnetwork.jenkins_stringutil.util;
 
 public class StringUtil {
-	
-	public static boolean isValidString(String stringValue) {
+
+    public static boolean isValidString(String stringValue) {
         boolean isValid = false;
         if ((stringValue != null) && (!(stringValue.trim().isEmpty())) && (!("null".equalsIgnoreCase(stringValue)))) {
             isValid = true;
